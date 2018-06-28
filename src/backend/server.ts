04649bd8ae3ce66cfd4155073787ea9724a82437
Charles Feval <chfeval@microsoft.com>
2018-06-27 17:48:08 -0700

    Fixing start

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,5 +1,12 @@
-import express from "express";
 import { App } from "./App";
+const PORT: number = ((): number => {
+    if (process.env["PORT"]) {
+        return +process.env["PORT"];
+    } else {
+        console.warn("No PORT env variable defined, defaulting to 8080");
+        return 8080;
+    };
+})();
 
 const app: App = new App();
-app.listen(8080);
+app.listen(PORT);
